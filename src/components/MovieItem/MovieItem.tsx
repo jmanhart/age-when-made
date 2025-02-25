e4a9a236c6715828b0601854d1d2ac4f1c2ea305
John Manhart <manhartjohn@gmail.com>
2025-02-24 19:15:07 -0800

    Fixing an error

--- src/components/MovieItem/MovieItem.tsx
+++ src/components/MovieItem/MovieItem.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Movie } from "../../types/types";
-
+import styles from "/src/components/MovieList/MovieList.module.css";
 interface MovieItemProps {
   movie: Movie;
 }
