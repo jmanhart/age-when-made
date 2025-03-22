622a50a1733e114a05d88d2faebed7534244261b
John Manhart <manhartjohn@gmail.com>
2025-03-22 09:16:36 -0700

    Adding in a whole bunch of styles with the actor card and making it less static fixing a bug with an icon that was hard coded as well. 💀 It Works... Somehow

--- src/components/ActorCard/ActorCard.tsx
+++ src/components/ActorCard/ActorCard.tsx
@@ -1,88 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styles from "./ActorCard.module.css";
-import "/src/styles/global.css";
+import { ActorCardProps } from "./types";
+import { ActorImage } from "./components/ActorImage";
+import { ActorHeader } from "./components/ActorHeader";
+import { ActorMetrics } from "./components/ActorMetrics";
+import classNames from "classnames";
 
-interface ActorProps {
-  id: number;
-  name: string;
-  character: string;
-  profilePath?: string;
-  birthday?: string;
-  deathday?: string;
-  currentAge?: number;
-  ageAtDeath?: number;
-  ageAtRelease?: number;
-}
-
-const ActorCard: React.FC<{ actor: ActorProps }> = ({ actor }) => {
-  const imageUrl = actor.profilePath
-    ? actor.profilePath.startsWith("http")
-      ? actor.profilePath
-      : `https://image.tmdb.org/t/p/w500${actor.profilePath}`
-    : "";
+const ActorCard: React.FC<ActorCardProps> = ({
+  actor,
+  variant = "default",
+  orientation = "vertical",
+  showMetrics = true,
+  showImage = true,
+  imageSize = "medium",
+  className,
+}) => {
+  const cardClasses = classNames(
+    styles.castItem,
+    {
+      [styles[`castItem--${variant}`]]: variant !== "default",
+      [styles[`castItem--${orientation}`]]: orientation !== "vertical",
+    },
+    className
+  );
 
   return (
-    <Link to={`/actor/${actor.id}`} className={styles.castItem}>
-      {imageUrl ? (
-        <img
-          src={imageUrl}
-          alt={`${actor.name}`}
-          className={`${styles.actorImage} ${
-            actor.deathday ? styles.deceased : ""
-          }`}
-          loading="lazy"
-        />
-      ) : (
-        <div className={`${styles.actorImage} ${styles.noImage}`}>
-          <span>No Image Available</span>
-        </div>
-      )}
+    <div className={cardClasses}>
+      {showImage && <ActorImage actor={actor} size={imageSize} />}
       <div className={styles.actorDetails}>
-        <header>
-          <h3 className={styles.actorName}>{actor.name}</h3>
-          <h4 className={styles.characterName}>{actor.character}</h4>
-        </header>
-
-        <div className={styles.metrics}>
-          {actor.birthday && (
-            <div className={styles.metricRow}>
-              <span className={styles.metricLabel}>Birthday</span>
-              <span className={styles.metricValue}>{actor.birthday}</span>
-            </div>
-          )}
-
-          {actor.deathday ? (
-            <>
-              <div className={styles.metricRow}>
-                <span className={styles.metricLabel}>Died</span>
-                <span className={styles.metricValue}>{actor.deathday}</span>
-              </div>
-              {actor.ageAtDeath && (
-                <div className={styles.metricRow}>
-                  <span className={styles.metricLabel}>Age at Death</span>
-                  <span className={styles.metricValue}>{actor.ageAtDeath}</span>
-                </div>
-              )}
-            </>
-          ) : (
-            actor.currentAge && (
-              <div className={styles.metricRow}>
-                <span className={styles.metricLabel}>Current Age</span>
-                <span className={styles.metricValue}>{actor.currentAge}</span>
-              </div>
-            )
-          )}
-
-          {actor.ageAtRelease && (
-            <div className={styles.metricRow}>
-              <span className={styles.metricLabel}>Age in Movie</span>
-              <span className={styles.metricValue}>{actor.ageAtRelease}</span>
-            </div>
-          )}
-        </div>
+        <ActorHeader actor={actor} />
+        {showMetrics && (
+          <ActorMetrics
+            actor={actor}
+            variant={variant === "detailed" ? "detailed" : "compact"}
+          />
+        )}
       </div>
-    </Link>
+    </div>
   );
 };
 
---
+++ src/components/ActorCard/components/ActorMetrics.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { ActorProps } from "../types";
+import styles from "../ActorCard.module.css";
+
+interface ActorMetricsProps {
+  actor: ActorProps;
+  variant?: "compact" | "detailed";
+  className?: string;
+}
+
+export const ActorMetrics: React.FC<ActorMetricsProps> = ({
+  actor,
+  variant = "default",
+  className,
+}) => {
+  return (
+    <div className={`${styles.metrics} ${className || ""}`}>
+      {actor.currentAge && (
+        <div className={styles.metricRow}>
+          <span className={styles.metricLabel}>Current Age:</span>
+          <span className={styles.metricValue}>{actor.currentAge}</span>
+        </div>
+      )}
+      {actor.ageAtRelease && (
+        <div className={styles.metricRow}>
+          <span className={styles.metricLabel}>Age in Movie:</span>
+          <span className={styles.metricValue}>{actor.ageAtRelease}</span>
+        </div>
+      )}
+      {actor.deathday && actor.ageAtDeath && (
+        <div className={styles.metricRow}>
+          <span className={styles.metricLabel}>Age at Death:</span>
+          <span className={styles.metricValue}>{actor.ageAtDeath}</span>
+        </div>
+      )}
+    </div>
+  );
+};
