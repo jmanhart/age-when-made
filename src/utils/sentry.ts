0a198b20264db549906020652f062b7ed89bbabb
John Manhart <manhartjohn@gmail.com>
2025-08-04 16:51:22 -0700

    Adding in session replay to my app 🫠 Melting Brain

--- src/utils/sentry.ts
+++ src/utils/sentry.ts
@@ -8,9 +8,18 @@ export const initSentry = () => {
       import.meta.env.VITE_SENTRY_DSN ||
       "https://cda3683d91776dea04de4020bf07a802@o4508215900766208.ingest.us.sentry.io/4508215907713024",
     tracesSampleRate: 1.0,
+    replaysSessionSampleRate: 1.0,
+    replaysOnErrorSampleRate: 1.0,
     environment: import.meta.env.MODE,
     debug: import.meta.env.DEV,
     release: __SENTRY_RELEASE__,
+    integrations: [
+      Sentry.replayIntegration({
+        // Additional SDK configuration goes in here, for example:
+        maskAllText: true,
+        blockAllMedia: true,
+      }),
+    ],
   });
 };
 
